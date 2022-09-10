119f58cd8ca15819f43a188e2165509ef2f9b9db
Heath Smith <smithmh6@outlook.com>
2022-09-10 16:23:16 -0400

    finish lab1

--- lab1_sqrt.asm
+++ lab1_sqrt.asm
@@ -1,17 +1,16 @@
 	.data
 guess:	.word	256		# initial guess value
 step:	.word	128		# initial step value
-test0:	.word	65536		# test value for initial guess squared
-res:	.word	22		# store expected end result
 
 	.text
-	#li	a7,5		# load sys call 5
-	#ecall			# execute sys call 5
-	li	a0,512
-	slli	a0,a0,14	# shift input left 14 bits
+	li	a7,5		# load sys call 5
+	ecall			# execute sys call 5
 	
-	slli	t0,a0,14	# shift input 14 bits left (32, 14) precision
+	#li	a0,61440
+	#slli	a0,a0,14	# shift input 14 bits left (32, 14) precision
 	
+	mv	t6,a0
+
 	lw	t1,guess	# load initial guess into t1
 	slli	t1,t1,14	# shift left 14 bits
 	
@@ -28,35 +27,19 @@ loop:	mul	t3,t1,t1	# square initial guess (low bits)
 	beq	a0,t5,exit	# exit if input == guess^2
 	blt	a0,t5,else	# branch if a0 < t5
 	add	t1,t1,t2	# add step if a0 > t5
+	b 	half		# jump to half
 
 else:	sub	t1,t1,t2	# jump here if a0 < t5
+	b 	half
+	
+half:	srli	t2,t2,1		# shift step right by 1 bit
 
-	srli	t2,t2,1		# shift step right by 1 bit
-	beq	t2,zero,exit		# stop if step == 0
+	beq	t2,zero,exit	# stop if step == 0
 	
-	b loop
+	b 	loop
 	
-	##### checkpoint - output expected result of multiplication #####
-	# li	a7,1		# load sys call 1 --> print integer
-	# lw	a0,test0	# load test value into a0
-	# slli	a0,a0,14	# shift left 14 bits
-	# ecall			# print value from a0 to console
-	# b exit
-	
-	##### checkpoint to see result of multiplication #####
-	# li	a7,1		# load sys call 1 --> print integer
-	# mv	a0,t5		# move value from t5 into a0
-	# ecall			# print value from a0 to console
-	
-	
-	b exit
+	b 	exit
 
-exit:	#li 	a7,1		# load sys call 1
-	#lw	t6,res
-	#slli	t6,t6,14
-	#ecall
-	
-	li 	a7,1		# load sys call 1
-	mv	a0,t5		# display raw result
-	#srli 	t5,t5,14	# shift t5 right 14 bits (equivalent to integer rounding)
+exit:	li 	a7,1		# load sys call 1
+	mv	a0,t1		# display raw result
 	ecall			# execute sys call (result should equal previous ecall)

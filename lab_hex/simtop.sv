f36b2a4836c18370cb37bf3237b8c7f01bd3a4dc
Heath Smith <heath.smith7792@gmail.com>
2022-10-07 17:19:17 -0400

    lab 2 submission

--- lab_hex/simtop.sv
+++ lab_hex/simtop.sv
@@ -7,13 +7,14 @@ module simtop;
 
 	logic clk;
 	logic [6:0] HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
+	logic [17:0] SW;
 
 	top dut
 	(
 		//////////// CLOCK //////////
 		.CLOCK_50(clk),
 		.CLOCK2_50(),
-	        .CLOCK3_50(),
+	   .CLOCK3_50(),
 
 		//////////// LED //////////
 		.LEDG(),
@@ -36,7 +37,72 @@ module simtop;
 		.HEX7(HEX7)
 	);
 
-// your code here
+	// your code here
+	initial begin
+		clk = 0; #5;
+		$display("-----------------------------");
+		
+		// HEX0
+		SW = 17'b00_0000_0000_0000_0000;
+		clk = 1; #5;
+		if (HEX0 === 7'b100_0000) $display("HEX0 test 1 PASS");
+		clk = 0; #5;
+		
+		SW = 17'b00_0000_0000_0000_0001;
+		clk = 1; #5;
+		if (HEX0 === 7'b111_1001) $display("HEX0 test 2 PASS");
+		clk = 0; #5;
+		$display("-----------------------------");
+
+		// HEX1
+		SW = 17'b00_0000_0000_0001_0000;
+		clk = 1; #5;
+		if (HEX1 === 7'b111_1001) $display("HEX1 test 1 PASS");
+		clk = 0; #5;
+		
+		SW = 17'b00_0000_0000_0100_0000;
+		clk = 1; #5;
+		if (HEX1 === 7'b001_1001) $display("HEX1 test 2 PASS");
+		clk = 0; #5;
+		$display("-----------------------------");
+		
+		// HEX2
+		SW = 17'b00_0000_1100_0000_0000;
+		clk = 1; #5;
+		if (HEX2 === 7'b100_0110) $display("HEX2 test 1 PASS");
+		clk = 0; #5;
+		
+		SW = 17'b00_0000_0011_0000_0000;
+		clk = 1; #5;
+		if (HEX2 === 7'b011_0000) $display("HEX2 test 2 PASS");
+		clk = 0; #5;
+		$display("-----------------------------");
+		
+		// HEX3
+		SW = 17'b00_1101_0000_0000_0000;
+		clk = 1; #5;
+		if (HEX3 === 7'b010_0001) $display("HEX3 test 1 PASS");
+		clk = 0; #5;
+		
+		SW = 17'b00_1001_0000_0000_0000;
+		clk = 1; #5;
+		if (HEX3 === 7'b001_1000) $display("HEX3 test 2 PASS");
+		clk = 0; #5;
+		$display("-----------------------------");
+		
+		// HEX4
+		SW = 17'b01_0000_0000_0000_0000;
+		clk = 1; #5;
+		if (HEX4 === 7'b111_1001) $display("HEX4 test 1 PASS");
+		clk = 0; #5;
+		
+		SW = 17'b00_0000_0000_0000_0000;
+		clk = 1; #5;
+		if (HEX4 === 7'b100_0000) $display("HEX4 test 2 PASS");
+		clk = 0; #5;
+		$display("-----------------------------");	
+		
+	end
 
 endmodule
 
--- lab_hex/top.sv
+++ lab_hex/top.sv
@@ -44,18 +44,26 @@ module top (
 	assign ledclk = clkdiv[23];
 
 	/* driver for LEDs */
-	logic [25:0] leds;
-	assign LEDR = leds[25:8];
-	assign LEDG = leds[7:0];
+	//logic [25:0] leds;
+	//assign LEDR = leds[25:8];
+	assign LEDR = SW;
+	//assign LEDG = leds[7:0];
+	assign LEDG = 8'b0000_0000;
 
 	/* LED state register, 0 means going left, 1 means going right */
 	logic ledstate;
 	
 	// default 7-seg value
-	logic [3:0] seg_default = 4'b0000;
-	logic [3:0] seg_5;  // used for the 5th display
-
-
+	logic [3:0] seg_0;
+	logic [3:0] seg_1;
+	logic [3:0] seg_2;
+	logic [3:0] seg_3;
+	logic [3:0] seg_4;
+	assign seg_0 = SW[3:0];
+	assign seg_1 = SW[7:4];
+	assign seg_2 = SW[11:8];
+	assign seg_3 = SW[15:12];
+	assign seg_4[1:0] = SW[17:16];
 //=======================================================
 //  Structural coding
 //=======================================================
@@ -64,32 +72,31 @@ module top (
 	initial begin
 		clkdiv = 26'h0;
 		/* start at the far right, LEDG0 */
-		leds = 26'b1;
+		//leds = 26'b1;
 		/* start out going to the left */
 		ledstate = 1'b0;
+		/* set unused bits of seg_4 */
+		seg_4[3:2] = 2'b00;
 	end
 
-	// hard wire two MSB in seg_5 to 0
-	assign seg_5[3:2] = 2'b00;
+
 	
-	// update LSB's in seg_5
-	assign seg_5[1:0] = SW[17:16];
 	
-	hexdriver hex_0(.val(SW[3:0]), .HEX(HEX0));
-	hexdriver hex_1(.val(SW[7:4]), .HEX(HEX1));
-	hexdriver hex_2(.val(SW[11:8]), .HEX(HEX2));
-	hexdriver hex_3(.val(SW[15:12]), .HEX(HEX3));
-	hexdriver hex_4(.val(seg_5), .HEX(HEX4));
-	hexdriver hex_5(.val(seg_default), .HEX(HEX5));
-	hexdriver hex_6(.val(seg_default), .HEX(HEX6));
-	hexdriver hex_7(.val(seg_default), .HEX(HEX7));
+	hexdriver hex_0(.val(seg_0), .HEX(HEX0));
+	hexdriver hex_1(.val(seg_1), .HEX(HEX1));
+	hexdriver hex_2(.val(seg_2), .HEX(HEX2));
+	hexdriver hex_3(.val(seg_3), .HEX(HEX3));
+	hexdriver hex_4(.val(seg_4), .HEX(HEX4));
+	hexdriver hex_5(.val(4'b0000), .HEX(HEX5));
+	hexdriver hex_6(.val(4'b0000), .HEX(HEX6));
+	hexdriver hex_7(.val(4'b0000), .HEX(HEX7));
 	
-	// always @(posedge CLOCK_50) begin
+	always @(posedge CLOCK_50) begin
 		/* drive the clock divider, every 2^26 cycles of CLOCK_50, the
 		* top bit will roll over and give us a clock edge for clkdiv
 		* */
-		// clkdiv <= clkdiv + 1;
-	// end
+		clkdiv <= clkdiv + 1;
+	end
 
 	// always @(posedge ledclk) begin	
 		/* going left and we are at the far left, time to turn around */
